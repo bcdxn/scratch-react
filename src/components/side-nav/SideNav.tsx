693d0903d8b0b8eb193acce779d38cef6ce6d4f5
bcdxn <5067649+bcdxn@users.noreply.github.com>
2025-04-16 22:28:32 -0400

    side-nav syles

--- src/components/side-nav/SideNav.tsx
+++ src/components/side-nav/SideNav.tsx
@@ -27,6 +27,50 @@ const CliReference: React.FC<CliReferenceProps> = (props) => {
           <h1>{props.doc.Info.Title}</h1>
           <Switch value={props.theme === Theme.DARK} onChange={toggleSwitch} />
         </div>
+
+        <h2>Introduction</h2>
+
+        <div className="nav-break"></div>
+
+        <h2>Install</h2>
+        <ul className="installation-methods">
+          <li>go install</li>
+          <li>homebrew</li>
+          <li>binary</li>
+        </ul>
+
+        <div className="nav-break"></div>
+
+        <h2>Commands</h2>
+        <ul className="command-group">
+          <li className="command">
+            <span className="command-name">ocli</span>
+            <ul className="command-group">
+              <li className="command">
+                <span className="command-name">generate</span>
+                <ul className="command-group">
+                  <li className="command">
+                    <span className="command-name">docs</span>
+                  </li>
+                  <li className="command">
+                    <span className="command-name">code</span>
+                  </li>
+                </ul>
+              </li>
+              <li className="command">
+                <span className="command-name">specification</span>
+                <ul className="command-group">
+                  <li className="command">
+                    <span className="command-name">check</span>
+                  </li>
+                  <li className="command">
+                    <span className="command-name">versions</span>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </>
   );
